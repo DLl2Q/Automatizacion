7bb610dfc0bd84717e8af42c84e31c978b25263b
DLlacsa <llacsa161@gmail.com>
2026-04-06 15:55:43 +0000

    Ignorar package-lock en ESLint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,9 @@
-import json from "@eslint/json";
-import { defineConfig } from "eslint/config";
-
-export default defineConfig([
-  { ignores: ["**/*.js", "**/*.cjs", "**/*.mjs"] },
-  { files: ["**/*.json"], plugins: { json }, language: "json/json", extends: ["json/recommended"] },
-]);
+export default [
+  {
+    ignores: [
+      "node_modules",
+      "package-lock.json",
+      "*.log"
+    ]
+  }
+];
